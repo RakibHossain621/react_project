64c4ba54e92aa8280a86a6208afd3052fc4db35b
RakibHossain621 <rakibhasanrasel07@gmail.com>
2024-08-07 13:53:51 +0600

    changes

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
-
+import PhoneSvg from './PhoneSvg'
+import MessageSvg from './MessageSvg'
+import LocationSvg from './LocationSvg'
+import FacebookSvg from './FacebookSvg'
 const ContactUs = () => {
   return (
-    <div>
-      hhhhh
+    <div className='container'>
+      <div className='text-center'>
+       <h1 className='text-[40px] font-bold text-[#2947A9] '>Contact Us</h1>
+      </div> 
+      <div className="contact_wrapper">
+        <div className="contact_left lg:w-[50%] w-[100%] ">
+          <h1 className='text-[32px] font-bold text-[#211F1E]'>Contract Details</h1>
+          <p className='text-[18px] font-semibold	text-[#211F1E] '>Please fill out and submit the contact form below and we will get back to you as soon as we can.</p>
+        </div>
+          <div className='flex gap-1'>
+              <PhoneSvg/>
+               <h1 className='text-[24.366px] font-medium	text-[#211F1E]'>0111 756 4973</h1>
+              </div>
+          <div className='flex gap-1'>
+              <PhoneSvg/>
+               <h1 className='text-[24.366px] font-medium	text-[#211F1E]'>0111 756 4973</h1>
+              </div>
+          <div className='flex gap-1'>
+              <MessageSvg/>
+               <h1 className='text-[24.366px] font-medium	text-[#211F1E]'>info@tho.com</h1>
+              </div>
+          <div className='flex gap-1'>
+              <LocationSvg/>
+               <h1 className='text-[24.366px] font-medium	text-[#211F1E]'>Black Lake  RoadKissimmee, 
+               FL 34747US</h1>
+              </div>
+          <div className='flex gap-1'>
+              <FacebookSvg/>
+               <h1 className='text-[24.366px] font-medium	text-[#211F1E]'>Follow Us</h1>
+              </div>
+        <div className="contact_right lg:w-[50%] w-[100%]">
+            <from
+            
+            />
+        </div>
+      </div>
     </div>
   )
 }
-
 export default ContactUs
---
+++ src/Components/ContactUs/MessageSvg.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const MessageSvg = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 31 32" fill="none">
+            <g clip-path="url(#clip0_1_1251)">
+                <path d="M29.5387 4.73938H0.918348C0.411764 4.73938 0 5.15114 0 5.65862V7.46647L15.2285 17.6711L30.4569 7.46551V5.65862C30.4569 5.15114 30.0452 4.73938 29.5387 4.73938Z" fill="#211F1E" />
+                <path d="M0 11.2346V23.3336C0 25.3635 1.64426 27.0078 3.67512 27.0078H26.7818C28.8127 27.0078 30.4569 25.3635 30.4569 23.3336V11.2336L15.2285 21.4393L0 11.2346Z" fill="#211F1E" />
+            </g>
+            <defs>
+                <clipPath id="clip0_1_1251">
+                    <rect width="30.4569" height="30.4569" fill="white" transform="translate(0 0.64502)" />
+                </clipPath>
+            </defs>
+        </svg>
+    )
+}
+
+export default MessageSvg
